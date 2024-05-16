bb6fd1acdd76c9cdc043e9560ab96130a9b20881
Robson Nayland Benjamim <robson.nayland@gmail.com>
2024-05-16 16:30:34 -0300

    more Bad solution

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -13,28 +13,38 @@ public class Program {
 
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Room number: ");
-		int number =  sc.nextInt();
+		int number = sc.nextInt();
 		System.out.println("Check-in date (dd/mm/yyyy):");
 		Date checkIn = sdf.parse(sc.next());
 		System.out.println("Check-Out date (dd/mm/yyyy):");
 		Date checkOut = sdf.parse(sc.next());
 		
-		String error = reservation.updateDates(checkIn,checkOut);
 		
+		Reservation reservation = new Reservation(number, checkIn, checkOut);
+
+		String error = reservation.updateDates(checkIn, checkOut);
+
 		if (error != null) {
-			System.out.println("error in reservation"+ error);
-		}else {
-			System.out.println("Reservation"+ reservation);
+			System.out.println("Error in reservation:" +error );
+		} else {
+
+			System.out.println("Reservation" + reservation);
+			System.out.println();
+			System.out.println("Enter data to update the reservation:");
+
+			System.out.println("Check-in date (dd/mm/yyyy):");
+			checkIn = sdf.parse(sc.next());
+			System.out.println("Check-Out date (dd/mm/yyyy):");
+			checkOut = sdf.parse(sc.next());
+
+			reservation.updateDates(checkIn, checkOut);
+
+			System.out.println("Reservation " + reservation);
 		}
-		
-		
-		
-		
-		
 		sc.close();
 
 	}
 
-} 
+}
